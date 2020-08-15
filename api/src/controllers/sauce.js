d704a00c89ffc90d96d332aef2cfe87d3e07425c
Kévin Monteiro <keiro.dev@gmail.com>
2020-08-16 00:11:51 +0200

    Implements likes & dislikes.

--- api/src/controllers/sauce.js
+++ api/src/controllers/sauce.js
@@ -14,7 +14,10 @@ exports.getAllSauces = (req, res) => {
 exports.getSauce = (req, res) => {
 
   Sauce.findOne({_id: req.params.id})
-    .then(sauce => res.status(httpStatus.OK).json(sauce))
+    .then(sauce => {
+      console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', sauce)
+      res.status(httpStatus.OK).json(sauce)
+    })
     .catch(error => res.status(httpStatus.BAD_REQUEST).json({error}))
 
 }
@@ -46,8 +49,8 @@ exports.updateSauce = (req, res) => {
     }
 
   Sauce.updateOne({_id: req.params.id}, finalSauce)
-    .then(() => res.status(200).json({message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({error}))
+    .then(() => res.status(httpStatus.OK).json({message: 'Objet modifié !'}))
+    .catch(error => res.status(httpStatus.BAD_REQUEST).json({error}))
 
 }
 
@@ -76,9 +79,92 @@ exports.deleteSauce = async (req, res) => {
     .catch(error => res.status(httpStatus.INTERNAL_SERVER_ERROR).json({error}))
 }
 
+/**
+ * @param req {{body: {userId: String, like: Number}, params: {id: String}}}
+ * @param res
+ */
 exports.handleLike = (req, res) => {
 
-  res.status(httpStatus.OK).json({message: 'handleLike'})
+  const sauceId = req.params.id
+
+  Sauce.findOne({_id: sauceId})
+    .then(sauce => {
+
+      // -1 = ajouter dans userDislike
+      // 0 = retirer si il existe danrs ld'un ou l'autre
+      // 1 ajouter userLikes
+      let usersLiked = sauce.usersLiked
+      let usersDisliked = sauce.usersDisliked
+      const userId = req.body.userId
+
+      /**
+       * Provide an abstraction to avoid duplication code while working with Arrays
+       * @param array {String[]}
+       * @param valueToRemove {String}
+       */
+      const removeFromArray = (array, valueToRemove) => {
+        let index = array.indexOf(valueToRemove)
+        if (index > -1) array.splice(index, 1)
+      }
+
+      let index
+      switch (req.body.like) {
+
+        case 0:
+          // L'user annule son choix, on le retire d'un tableau si il y est
+          // utilisateur a déjà liké ?
+          removeFromArray(usersLiked, userId)
+          // utilisateur a déjà disliké ?
+          removeFromArray(usersDisliked, userId)
+          break
+
+        case 1:
+          // utilisateur à déjà liké ?
+          index = usersLiked.indexOf(userId)
+          // il n'a pas le droit de reliker ! on stop et informe le front
+          if (index > -1) return Promise.resolve('Cet a tilisateur a déja liké ça !')
+
+          // utilisateur a déjà disliké ? Si oui, on le retire du tableau
+          removeFromArray(usersDisliked, userId)
+          // puis l'ajoute dans le like
+          usersLiked.push(userId)
+          break
+
+        case -1:
+          // utilisateur à déjà disliké ?
+          index = usersDisliked.indexOf(userId)
+          // il n'a pas le droit de re-disliker ! on stop et informe le front
+          if (index > -1) return Promise.resolve('Cet utilisateur a déja disliké ça !')
+
+          // utilisateur a déjà liké ? Si oui, on le retire du tableau
+          removeFromArray(usersLiked, userId)
+
+          // puis on l'ajoute dans le dislike
+          usersDisliked.push(userId)
+          break
+      }
+
+      const likes = usersLiked.length
+      const dislikes = usersDisliked.length
+
+
+      // Build new object from this new one
+      const sauceObject = {
+        ...sauce._doc,
+        _id: sauceId,
+        likes,
+        dislikes,
+        usersLiked,
+        usersDisliked
+      }
+
+      // return final object to update
+      return sauceObject
+
+    })
+    .then(sauce => Sauce.updateOne({_id: sauceId}, sauce))
+    .then(() => res.status(httpStatus.OK).json({message: '(Dis-)Likes updated !'}))
+    .catch(error => res.status(httpStatus.BAD_REQUEST).json({message: error}))
 
 }
 
